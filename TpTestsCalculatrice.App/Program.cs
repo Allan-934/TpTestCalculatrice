029c83f5edf2f15ed2a3588a995d2fae63efa6bf
Decugis Allan <allan.decugis@edouardgand.fr>
2026-04-02 15:46:36 +0200

    Ajout de la class test calculatrice

--- TpTestsCalculatrice.App/Program.cs
+++ TpTestsCalculatrice.App/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Calculatrice calc = new Calculatrice();
+
+        Console.WriteLine(calc.Addition(2, 3));
+        Console.WriteLine(calc.Division(10, 2)); 
+
+        // Le test manuel n'est pas suffisant car il faut effectuer un test avant 
+        
     }
 }
---
+++ TpTestsCalculatrice.Tests/CalculatriceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting; 
+using TpTestsCalculatrice.App; 
+ 
+namespace TpTestsCalculatrice.Tests; 
+ 
+[TestClass] 
+public class CalculatriceTests 
+{ 
+    [TestMethod] 
+    public void Addition_Retourne5_Si2Plus3() 
+    { 
+        Calculatrice calc = new Calculatrice(); 
+ 
+        int resultat = calc.Addition(2, 3); 
+ 
+        Assert.AreEqual(5, resultat); 
+    } 
+
+    // 1 test effectue ; 0 test echoue ; 1 test reussi  
+}
