a54ec6615dd3f35677b49f5c8769d1773a45a5af
Decugis Allan <allan.decugis@edouardgand.fr>
2026-04-02 15:55:25 +0200

    Part 5 exo 1 et 2 Soustraction et multiplication

--- TpTestsCalculatrice.Tests/CalculatriceTest.cs
+++ TpTestsCalculatrice.Tests/CalculatriceTest.cs
@@ -14,7 +14,24 @@ public class CalculatriceTests
         int resultat = calc.Addition(2, 3); 
  
         Assert.AreEqual(5, resultat); 
-    } 
-
+    }
     // 1 test effectue ; 0 test echoue ; 1 test reussi  
+
+    //Part 5 Ex1 
+    [TestMethod]
+    public void Soutraction_5Moins3()
+    {
+        Calculatrice c2 = new Calculatrice ();
+        int res2 = c2.Soustraction(5, 3);
+        Assert.AreEqual(2, res2);
+    }
+
+    //Part 5 Ex2 
+    [TestMethod]
+    public void Multiplication_4x3()
+    {
+        Calculatrice c3 = new Calculatrice ();
+        int res3 = c3.Multiplication(4, 3);
+        Assert.AreEqual(12, res3);
+    }
 }
