5b050307507920455276c471ae0e828e78e91240
Decugis Allan <allan.decugis@edouardgand.fr>
2026-04-02 16:29:16 +0200

    Part 6 Et 7

--- TpTestsCalculatrice.Tests/CalculatriceTest.cs
+++ TpTestsCalculatrice.Tests/CalculatriceTest.cs
@@ -1,19 +1,19 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting; 
-using TpTestsCalculatrice.App; 
- 
-namespace TpTestsCalculatrice.Tests; 
- 
-[TestClass] 
-public class CalculatriceTests 
-{ 
-    [TestMethod] 
-    public void Addition_Retourne5_Si2Plus3() 
-    { 
-        Calculatrice calc = new Calculatrice(); 
- 
-        int resultat = calc.Addition(2, 3); 
- 
-        Assert.AreEqual(5, resultat); 
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TpTestsCalculatrice.App;
+
+namespace TpTestsCalculatrice.Tests;
+
+[TestClass]
+public class CalculatriceTests
+{
+    [TestMethod]
+    public void Addition_Retourne5_Si2Plus3()
+    {
+        Calculatrice calc = new Calculatrice();
+
+        int resultat = calc.Addition(2, 3);
+
+        Assert.AreEqual(5, resultat);
     }
     // 1 test effectue ; 0 test echoue ; 1 test reussi  
 
@@ -21,7 +21,7 @@ public class CalculatriceTests
     [TestMethod]
     public void Soutraction_5Moins3()
     {
-        Calculatrice c2 = new Calculatrice ();
+        Calculatrice c2 = new Calculatrice();
         int res2 = c2.Soustraction(5, 3);
         Assert.AreEqual(2, res2);
     }
@@ -30,7 +30,7 @@ public class CalculatriceTests
     [TestMethod]
     public void Multiplication_4x3()
     {
-        Calculatrice c3 = new Calculatrice ();
+        Calculatrice c3 = new Calculatrice();
         int res3 = c3.Multiplication(4, 3);
         Assert.AreEqual(12, res3);
     }
@@ -39,8 +39,38 @@ public class CalculatriceTests
     [TestMethod]
     public void Division_10Par2()
     {
-        Calculatrice c4 = new Calculatrice ();
-        double res4 = c4.Division(10, 2); 
+        Calculatrice c4 = new Calculatrice();
+        double res4 = c4.Division(10, 2);
         Assert.AreEqual(5, res4);
+    }
+
+    //Part 6 
+    [TestMethod]
+    public void Division_10Par0()
+    {
+        Calculatrice c5 = new Calculatrice();
+
+        Assert.Throws<Exception>(() => c5.Division(10, 0));
+    }
+
+    //Part 7 True 
+    [TestMethod]
+    public void Test_booleenTrue()
+    {
+        Calculatrice c6 = new Calculatrice();
+        bool r6 = c6.EstPair(4);
+
+        Assert.IsTrue(r6);
+        
+    }
+
+    //Part 7 False
+    [TestMethod]
+    public void Test_booleenfalse()
+    {
+        Calculatrice c7 = new Calculatrice();
+        bool r7 = c7.EstPair(5);
+
+        Assert.IsFalse(r7);
     }
 }
